ebbbe66d73bfb6cfb0f35aaea5b401253d3a128c
Jaimin Ashwin Maniar <129639641+jaimin-maniar@users.noreply.github.com>
2025-01-13 16:21:50 +0530

    Update orders.js

--- scripts/orders.js
+++ scripts/orders.js
@@ -154,7 +154,7 @@ function renderOrders() {
   document.querySelector('.js-search-bar').addEventListener('keydown', (event) => {
 
     if (event.key === 'Enter') {
-      const search = document.querySelector('.js-search-bar').value.toLowerCase();
+      const search = document.querySelector('.js-search-bar').value;
       window.location.href = `index.html?search=${search}`;
     }
   });
